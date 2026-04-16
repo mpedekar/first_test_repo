132161d083c355a73c31fa8b0977bd2d3eec3836
mpedekar <mpedekar@sscinc.com>
2026-04-16 16:36:26 +0530

    add ou info in the script

--- Active Directory/Export_AD_Data_With_ADWS.ps1
+++ Active Directory/Export_AD_Data_With_ADWS.ps1
@@ -1,9 +1,8 @@
 # Specify the domain or domain controller here
 $DomainController = "ad.dstsystems.com"   # <-- change this as needed
-
 # Query all servers from the specified AD domain and export to CSV with domain name and timestamp
 Get-ADComputer -Server $DomainController `
-    -Properties OperatingSystem, ServicePrincipalName, DNSHostName, IPv4Address `
+    -Properties OperatingSystem, ServicePrincipalName, DNSHostName, IPv4Address, DistinguishedName `
     -LDAPFilter '(operatingsystem=*Server*)' |
 Select-Object `
     Name, 
@@ -21,7 +20,16 @@ Select-Object `
                 $false
             }
         } 
+    },
+    @{ 
+        Name = 'OU';
+        Expression = {
+            # Extract only OU components from DistinguishedName and join with '/'
+            if ($_.DistinguishedName) {
+                ($_.DistinguishedName -split ',' | ForEach-Object { $_.Trim() } | Where-Object { $_ -like 'OU=*' } | ForEach-Object { $_ -replace '^OU=', '' }) -join '/'
+            } else {
+                ''
+            }
+        }
     } |
-Export-CSV "C:\temp\$($DomainController.Replace('.', '_'))-clusterobjects-$(Get-Date -Format 'yyyyMMdd_HHmmss').csv" `
-    -NoTypeInformation `
-    -Encoding UTF8
+Export-CSV "C:\temp\$($DomainController.Replace('.', '_'))-clusterobjects-$(Get-Date -Format 'yyyyMMdd_HHmmss').csv" -NoTypeInformation -Encoding UTF8
